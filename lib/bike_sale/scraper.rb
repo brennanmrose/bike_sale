f878caa63864b69d649fb113ce36fbabd23d8b90
github username <github email address>
2018-12-19 07:21:59 -0500

    set up bin

--- lib/bike_sale/scraper.rb
+++ lib/bike_sale/scraper.rb
@@ -2,7 +2,7 @@ class BikeSale::Scraper
 
 	def self.scrape_bikes
 		index_page = Nokogiri::HTML(open("https://boston.craigslist.org/d/bicycles/search/bia"))
-
+binding.pry
 		array_of_bikes = index_page.css()
 
 	end
