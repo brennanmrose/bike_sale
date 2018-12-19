3982bf663e6900dbdf080d52f36bc1165db129c3
github username <github email address>
2018-12-19 07:16:29 -0500

    set up scraper

---
+++ lib/bike_sale/scraper.rb
@@ -0,0 +1,11 @@
+class BikeSale::Scraper 
+
+	def self.scrape_bikes
+		index_page = Nokogiri::HTML(open("https://boston.craigslist.org/d/bicycles/search/bia"))
+
+		array_of_bikes = index_page.css()
+
+	end
+
+
+end
