fcf1c132722eccd4c694ba922dd508629486298b
github username <github email address>
2018-12-21 07:14:43 -0500

    update cli functionality

--- lib/bike_sale/cli.rb
+++ lib/bike_sale/cli.rb
@@ -3,16 +3,48 @@ class BikeSale::CLI
 
 	def start 
 		puts "Welcome to the Bike Sale CLI!"
-		puts "Here are all of the current bikes for sale on Boston Craigslist:"
 		BikeSale::Scraper.scrape_bikes
 		list_bikes
+		bike_details
+		menu
 	end
 
 	def list_bikes
+		puts "Here are all of the current bikes for sale on Boston Craigslist:"
 		BikeSale::Bikes.all.each.with_index(1) do |bike, index|
-			puts 
-			puts "#{index}. #{bike.title.capitalize}"
+			puts "\n#{index}. #{bike.title.capitalize}"
 		end
 	end
+
+	def menu 
+		puts "\nWhat would you like to do next? Please select a number from the following:"
+		puts "1. Learn more about this bike"
+			# learn_more
+		puts "2. Select a different bike"
+			# list_bikes
+		puts "3. End CLI"
+			# end_cli 
+	end
+
+	def bike_details
+		puts "\nPlease select the number of a bike you'd like to read more about:"
+		input = gets.strip
+		index = input.to_i-1 
+		if index.between?(0,5)
+			bike = BikeSale::Bikes.all[index]
+			puts "You have selected '#{bike.title}', posted on #{bike.date_posted}. This bike is priced at #{bike.price} and located near #{bike.location}."
+			menu 
+		elsif 
+			input == "exit"
+		else 
+			puts "Sorry, that input cannot be found"
+			bike_details
+		end
+	end
+
+	def learn_more
+		#second layer scrape
+	end
+
 
 end
--- lib/bike_sale/scraper.rb
+++ lib/bike_sale/scraper.rb
@@ -4,26 +4,20 @@ class BikeSale::Scraper
 		index_page = Nokogiri::HTML(open("https://boston.craigslist.org/d/bicycles/search/bia"))
 
 		array_of_bikes = index_page.css("ul.rows li.result-row")
-			array_of_bikes.each do |bike_card|
-				attributes = {
-					title: bike_card.css(".result-title")[0].text,
-					price: bike_card.css(".result-price")[0].text,
-					date_posted: bike_card.css(".result-date")[0].text,  
-					location: bike_card.css(".result-hood")[0].text,
-				}
-				attributes.fetch(:title, "none_provided")
-				attributes.fetch(:price, "none_provided")
-				attributes.fetch(:date_posted, "none_provided") 
-				attributes.fetch(:location, "none_provided") 
-
+		array_of_bikes[0..5].each do |bike_card|
+			attributes = {
+				title: bike_card.css(".result-title")[0].text,
+				# price: bike_card.css(".result-price")[0].text,
+				date_posted: bike_card.css(".result-date")[0].text,  
+				# location: bike_card.css(".result-hood")[0].text,
+			}
+			attributes.fetch(:title, "none_provided")
+			attributes.fetch(:price, "none_provided")
+			attributes.fetch(:date_posted, "none_provided") 
+			attributes.fetch(:location, "none_provided") 
 
 			bike = BikeSale::Bikes.new(attributes)
-			end
-		
+		end	
 	end
 
-
 end
-
-
-# index_page.css("ul.rows").css(".result-title")[0].text
